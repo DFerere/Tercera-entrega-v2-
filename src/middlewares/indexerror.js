8fa3f8854b077d3198a150b230b78010f28c76ca
David Ferere <davidferere@gmail.com>
2023-11-19 19:15:04 -0400

    Update indexerror.js

--- src/middlewares/indexerror.js
+++ src/middlewares/indexerror.js
@@ -9,7 +9,7 @@ export default (error, req, res, next) => {
             break; 
             
         default:
-            res.send("Error no manejado"); 
+            res.send({status:"error", error:"Error no manejado"}); 
 
     }
 
